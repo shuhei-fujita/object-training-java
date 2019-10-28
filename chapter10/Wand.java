e6e0c7c6880e00c5e38421b81acf315ef55243f5
syuheifujita <mcslr772ed@gmail.com>
2019-10-28 20:32:08 +0900

    getter,setterを定義

--- chapter10/Wand.java
+++ chapter10/Wand.java
@@ -3,4 +3,18 @@
 public class Wand {
     private String name;
     private double power;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- chapter10/Wizard.java
+++ chapter10/Wizard.java
@@ -11,4 +11,60 @@ public class Wizard {
         h.setHp(h.getHp() + recovPoint);
         System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
     }
+
+    /**
+     * @return the hp
+     */
+    public int getHp() {
+        return this.hp;
+    }
+
+    /**
+     * @return the mp
+     */
+    public int getMp() {
+        return this.mp;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * @return the wand
+     */
+    public Wand getWand() {
+        return this.wand;
+    }
+
+    /**
+     * @param hp the hp to set
+     */
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    /**
+     * @param mp the mp to set
+     */
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param wand the wand to set
+     */
+    public void setWand(Wand wand) {
+        this.wand = wand;
+    }
 }
