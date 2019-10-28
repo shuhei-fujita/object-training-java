62ace27cce56e6a0e6b1e01eb280222ca5f0b0b8
syuheifujita <mcslr772ed@gmail.com>
2019-10-28 18:29:16 +0900

    prayメソッドの処理を実装

--- Cleric.java
+++ Cleric.java
@@ -4,14 +4,29 @@ public class Cleric {
     final int maxHp = 50;
     int mp = 10;
     final int maxMp = 10;
-    void selfAid() {
+
+    public void selfAid() {
         System.out.println("聖職者は「セルフエイド」を唱えた！");
         mp = mp - 5;
         hp = maxHp;
         System.out.println("HPが最大まで回復した！");
     }
 
-    void pray() {
-        
+    public int pray(int prayTime) {
+        // 祈る呪文を発動
+        System.out.println("聖職者は" + prayTime + "秒間「祈る」をした！");
+
+        // 回復値をランダムを使って求める
+        int aidPoint = new java.util.Random().nextInt(2) + prayTime;
+
+        // 実際の回復量を計算
+        // 最大MPより大きくならないという条件付き
+        int upMp = Math.min(maxMp - mp, aidPoint);
+
+        // mpの値を更新
+        mp += upMp;
+
+        // 戻り値を回復量を返す
+        return upMp;
     }
 }
