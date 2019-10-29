c2272def452bea986b32673c3a8391e5cb8c81bb
syuheifujita <mcslr772ed@gmail.com>
2019-10-29 12:49:52 +0900

    メソッドのオーバーライドを実装

--- ch11/PoisonMatango.java
+++ ch11/PoisonMatango.java
@@ -8,6 +8,22 @@ public class PoisonMatango extends Matango {
         // TODO Auto-generated constructor stub
     }
 
+    public void attack() {
+        System.out.println("きのこ" + this.suffix + "の攻撃");
+        System.out.println("10のダメージ");
+        h.setHp(getHp() - 10);
+
+        if (poisonCount != 0) {
+            attackPoison(this.h);
+            System.out.println("毒の胞子をばらまいた!");
+
+            steHp(hp - hp / 5);
+            System.out.println(hp / 5 + "ポイントのダメージ");
+
+            poisonCount--;
+        }
+    }
+
     // 毒攻撃ができるので
     public void attackPoison(Hero h) {
         System.out.println("きのこ" + this.suffix + "の攻撃");
