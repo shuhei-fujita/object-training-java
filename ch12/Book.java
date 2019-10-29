dc97563585ec8e482b64b5d888a1681a3a1e8f4f
syuheifujita <mcslr772ed@gmail.com>
2019-10-29 16:56:49 +0900

    修正

--- ch12/Book.java
+++ ch12/Book.java
@@ -1,39 +1,13 @@
 public class Book extends TangibleAsset {
-    private String name;
-    private int price;
-    private String color;
     private String isbn;
 
     // コンストラクタ
     public Book(String name, int price, String color, String isbn) {
-        this.name = name;
-        this.price = price;
-        this.color = color;
+        super(name, price, color);
         this.isbn = isbn;
     }
 
     // getterメソッド
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return this.name;
-    }
-
-    /**
-     * @return the price
-     */
-    public int getPrice() {
-        return this.price;
-    }
-
-    /**
-     * @return the color
-     */
-    public String getColor() {
-        return this.color;
-    }
-
     /**
      * @return the isbn
      */
--- ch12/TangibleAsset.java
+++ ch12/TangibleAsset.java
@@ -1,3 +1,32 @@
-public abstract class TangibleAsset {
+public abstract class TangibleAsset extends Asset implements Thing {
+    private String name;
+    private int price;
+    private String color;
 
+    public TangibleAsset(String name, int price, String color) {
+        this.name = name;
+        this.price = price;
+        this.color = color;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return this.price;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return this.color;
+    }
 }
