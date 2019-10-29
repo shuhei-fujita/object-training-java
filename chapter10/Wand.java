ac6c78d9bafd53fcb0a989799914873ffa4dcbbc
syuheifujita <mcslr772ed@gmail.com>
2019-10-29 12:37:12 +0900

    練習問題ー継承ーまず親クラス，小クラスを定義した

--- chapter10/Wand.java
+++ chapter10/Wand.java
@@ -12,9 +12,23 @@ public class Wand {
     }
 
     /**
+     * @return the power
+     */
+    public double getPower() {
+        return this.power;
+    }
+
+    /**
      * @param name the name to set
      */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * @param power the power to set
+     */
+    public void setPower(double power) {
+        this.power = power;
     }
 }
