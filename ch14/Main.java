3d122859f694a76b482302a2f48d5cd8193f21aa
syuheifujita <mcslr772ed@gmail.com>
2019-10-29 21:23:26 +0900

    Javaのいろいろなクラスを触ってみる試み

---
+++ ch14/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        long nowTime = System.currentTimeMillis();
+        System.out.println(nowTime);
+
+        Date d = new Date();
+        System.out.println(d);
+    }
+}
