90b962ff89bd4c2408d925f742388283659861e3
syuheifujita <mcslr772ed@gmail.com>
2019-10-29 22:31:26 +0900

    Calendarクラスの利用

--- ch14/Main.java
+++ ch14/Main.java
@@ -1,11 +1,15 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        long nowTime = System.currentTimeMillis();
-        System.out.println(nowTime);
+        // 日時をDate型で取得
+        Date now = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(now);
 
-        Date d = new Date();
-        System.out.println(d);
+        int year = c.get(Calendar.YEAR);
+        System.out.println("今年は" + year + "年です");
     }
 }
