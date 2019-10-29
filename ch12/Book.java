239ab2cb162200b8c18236133fb0d69f197e25c3
syuheifujita <mcslr772ed@gmail.com>
2019-10-29 16:34:52 +0900

    Book.javaのメンバを記述

--- ch12/Book.java
+++ ch12/Book.java
@@ -1,3 +1,43 @@
 public class Book extends TangibleAsset {
+    private String name;
+    private int price;
+    private String color;
+    private String isbn;
 
+    // コンストラクタ
+    public Book(String name, int price, String color, String isbn) {
+        this.name = name;
+        this.price = price;
+        this.color = color;
+        this.isbn = isbn;
+    }
+
+    // getterメソッド
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return this.price;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return this.color;
+    }
+
+    /**
+     * @return the isbn
+     */
+    public String getIsbn() {
+        return this.isbn;
+    }
 }
