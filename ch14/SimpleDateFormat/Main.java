9993eff78b9c99b0113068c0f540c7f33be045a8
syuheifujita <mcslr772ed@gmail.com>
2019-10-30 15:54:34 +0900

    コメント入れ

--- ch14/SimpleDateFormat/Main.java
+++ ch14/SimpleDateFormat/Main.java
@@ -3,12 +3,13 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Date now = new Date();
-        SimpleDateFormat f = new SimpleDateFormat();
+        Date now = new Date(); // まずDateクラスを使って，Date型の現在時刻を取り出す
 
+        SimpleDateFormat f = new SimpleDateFormat(); // Date型からString型を生成
         String s = f.format(now);
         System.out.println(s);
 
         Date d = f.parse("2011/09/22 01:23:45");
+        System.out.println(d);
     }
 }
